d49785f49d70cb1b96e5ec40f3035e0a1a987318
windboy12138 <sduwangxiaohui@hotmail.com>
2024-06-28 14:57:25 +0800

    CameraNative: modify code format and video render rotation

--- hirtc_camera_native/entry/src/main/cpp/ohos_camera.h
+++ hirtc_camera_native/entry/src/main/cpp/ohos_camera.h
@@ -11,8 +11,10 @@
 #include "hilog/log.h"
 #include "napi/native_api.h"
 #include <arm-linux-ohos/bits/alltypes.h>
+#include <cstdint>
 #include <multimedia/image_framework/image_receiver_mdk.h>
 #include <ohcamera/camera.h>
+#include <ohcamera/camera_device.h>
 #include <ohcamera/camera_manager.h>
 #include <multimedia/image_framework/image/image_receiver_native.h>
 #include <multimedia/image_framework/image/image_common.h>
@@ -27,99 +29,103 @@
 // #include "modules/video_capture/video_capture.h"
 
 
-namespace webrtc{
-namespace ohos{
+namespace webrtc {
+namespace ohos {
+
+struct VideoFrame {
+	uint8_t *buffer;
+	uint32_t width;
+	uint32_t height;
+	uint32_t stride;
+	uint32_t orientation;
+};
+
 class OhosCamera {
 public:
-  OhosCamera();
-  static bool Init(napi_env env, napi_callback_info info);
-  int32_t InitCamera();
-  int32_t InitImageReceiver();
-  int32_t StartCamera();
-  int32_t StopCamera();
-  int32_t CameraRelease();
-  
-  uint32_t GetCameraIndex();
-  int32_t SetCameraIndex(uint32_t camera_index);
-  napi_value GetImageData(napi_env env, ImageReceiverNative *image_receiver_c);
-  bool ImageReceiverOn(uint8_t *buffer, int32_t width, int32_t height, int32_t stride, size_t bufferSize);
-//   void RegisterCaptureDataCallback(rtc::VideoSinkInterface<webrtc::VideoFrame> *dataCallback);
-  void UnregisterCaptureDataCallback();
-  ~OhosCamera();
-    
-    void RunRenderProcess();
+	OhosCamera();
+	static bool Init(napi_env env, napi_callback_info info);
+	int32_t InitCamera();
+	int32_t InitImageReceiver();
+	int32_t StartCamera();
+	int32_t StopCamera();
+	int32_t CameraRelease();
+
+	uint32_t GetCameraIndex();
+	int32_t SetCameraIndex(uint32_t camera_index);
+	napi_value GetImageData(napi_env env, ImageReceiverNative *image_receiver_c);
+	bool ImageReceiverOn(uint8_t *buffer, int32_t width, int32_t height, int32_t stride, size_t bufferSize);
+	//   void RegisterCaptureDataCallback(rtc::VideoSinkInterface<webrtc::VideoFrame> *dataCallback);
+	void UnregisterCaptureDataCallback();
+	~OhosCamera();
+
+	void RunRenderProcess();
 
 private:
-  int32_t CameraInputCreateAndOpen();
-  int32_t CameraInputRelease();
-  
-  int32_t PreviewOutputCreate();
-  int32_t PreviewOutputRelease();
-  
-  int32_t CaptureSessionSetting();
-  int32_t CaptureSessionUnsetting();
-  
-  int32_t ImageReceiverRelease();
+	int32_t CameraInputCreateAndOpen();
+	int32_t CameraInputRelease();
 
-  int32_t DeleteCameraOutputCapability();
-  int32_t DeleteCameras();
-  int32_t DeleteCameraManage();
-  
-//   rtc::VideoSinkInterface<webrtc::VideoFrame> *data_callback_ {nullptr};
-//   webrtc::VideoCaptureCapability configured_capability_;
-  bool is_camera_started_ {false};
-  char* img_receive_surfaceId_ {nullptr};
-  static char* x_component_surfaceId_;
-  OH_ImageReceiverNative *imageReceiverNative_ {nullptr};
-  Camera_Manager* camera_manager_ {nullptr};
-  Camera_Device* cameras_ {nullptr};
-  uint32_t cameras_size_ {0};
-  Camera_CaptureSession* capture_session_ {nullptr};
-  Camera_OutputCapability* camera_output_capability_ {nullptr};
-  const Camera_Profile* preview_profile_ {nullptr};
-  Camera_PreviewOutput* preview_output_ {nullptr};
-  Camera_PreviewOutput* x_component_preview_ {nullptr};
-  Camera_Input* camera_input_ {nullptr};
-  uint32_t camera_dev_index_ {0};
-  uint32_t profile_index_ {61};
-  ohosRender* ohos_render_ {nullptr};
+	int32_t PreviewOutputCreate();
+	int32_t PreviewOutputRelease();
+
+	int32_t CaptureSessionSetting();
+	int32_t CaptureSessionUnsetting();
+
+	int32_t ImageReceiverRelease();
+
+	int32_t DeleteCameraOutputCapability();
+	int32_t DeleteCameras();
+	int32_t DeleteCameraManage();
+
+	//   rtc::VideoSinkInterface<webrtc::VideoFrame> *data_callback_ {nullptr};
+	//   webrtc::VideoCaptureCapability configured_capability_;
+	bool is_camera_started_{false};
+	char *img_receive_surfaceId_{nullptr};
+	static char *x_component_surfaceId_;
+	OH_ImageReceiverNative *imageReceiverNative_{nullptr};
+	Camera_Manager *camera_manager_{nullptr};
+	Camera_Device *cameras_{nullptr};
+	uint32_t cameras_size_{0};
+	Camera_CaptureSession *capture_session_{nullptr};
+	Camera_OutputCapability *camera_output_capability_{nullptr};
+	const Camera_Profile *preview_profile_{nullptr};
+	Camera_PreviewOutput *preview_output_{nullptr};
+	Camera_PreviewOutput *x_component_preview_{nullptr};
+	Camera_Input *camera_input_{nullptr};
+	uint32_t camera_dev_index_{0};
+	uint32_t profile_index_{61};
+	ohosRender *ohos_render_{nullptr};
+	uint32_t camera_orientitaion_{0};
 };
 class GlobalOHOSCamera {
 public:
-    static GlobalOHOSCamera& GetInstance() {
-        static GlobalOHOSCamera globalOHOSCamera;
-        return globalOHOSCamera;
-    }
-    
-    ~GlobalOHOSCamera()
-    {
-        OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "GlobalOHOSCamera::~GlobalOHOSCamera start");
-        if (ohosCamera_)
-        {
-            delete ohosCamera_;
-            ohosCamera_ = nullptr;
-        }
-    }
-    
-    void SetOhosCamera(OhosCamera* ohosCamera) {
-        ohosCamera_ = ohosCamera;
-    }
-    
-    void UnsetOhosCamera() {
-        ohosCamera_ = nullptr;
-    }
-    
-    OhosCamera* GetOhosCamera() {
-        if (ohosCamera_ == nullptr)
-        {
-            ohosCamera_ = new OhosCamera;
-        }
-        return ohosCamera_;
-    }
+	static GlobalOHOSCamera &GetInstance() {
+		static GlobalOHOSCamera globalOHOSCamera;
+		return globalOHOSCamera;
+	}
+
+	~GlobalOHOSCamera() {
+		OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "GlobalOHOSCamera::~GlobalOHOSCamera start");
+		if (ohosCamera_) {
+			delete ohosCamera_;
+			ohosCamera_ = nullptr;
+		}
+	}
+
+	void SetOhosCamera(OhosCamera *ohosCamera) { ohosCamera_ = ohosCamera; }
+
+	void UnsetOhosCamera() { ohosCamera_ = nullptr; }
+
+	OhosCamera *GetOhosCamera() {
+		if (ohosCamera_ == nullptr) {
+			ohosCamera_ = new OhosCamera;
+		}
+		return ohosCamera_;
+	}
+
 private:
-    GlobalOHOSCamera() {};
-    OhosCamera* ohosCamera_ {nullptr};
+	GlobalOHOSCamera(){};
+	OhosCamera *ohosCamera_{nullptr};
 };
 } // namespace ohos
 } // namespace webrtc
-#endif //OH_WEB_RTC_OHOS_CAMERA_H
+#endif // OH_WEB_RTC_OHOS_CAMERA_H
--- hirtc_camera_native/entry/src/main/cpp/ohos_render.cpp
+++ hirtc_camera_native/entry/src/main/cpp/ohos_render.cpp
@@ -11,231 +11,239 @@
 
 #include "hilog/log.h"
 
-namespace webrtc{
+namespace webrtc {
 namespace ohos {
 
-    void PluginManager::Export(napi_env env, napi_value exports) 
-    {
-        OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "Export: PluginManager");
-        if ((env == nullptr) || (exports == nullptr)) {
-            OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, "mytest", "Export: env or exports is null");
-            return;
-        }
-    
-        napi_value exportInstance = nullptr;
-        if (napi_get_named_property(env, exports, OH_NATIVE_XCOMPONENT_OBJ, &exportInstance) != napi_ok) 
-        {
-            OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, "mytest", "Export: napi_get_named_property fail");
-            return;
-        }
-    
-        OH_NativeXComponent *nativeXComponent = nullptr;
-        if (napi_unwrap(env, exportInstance, reinterpret_cast<void **>(&nativeXComponent)) != napi_ok) 
-        {
-            OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, "mytest", "Export: napi_unwrap fail");
-            return;
-        }
-    
-        char idStr[OH_XCOMPONENT_ID_LEN_MAX + 1] = {'\0'};
-        uint64_t idSize = OH_XCOMPONENT_ID_LEN_MAX + 1;
-        if (OH_NativeXComponent_GetXComponentId(nativeXComponent, idStr, &idSize) != OH_NATIVEXCOMPONENT_RESULT_SUCCESS) 
-        {
-            OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, "mytest", "Export: OH_NativeXComponent_GetXComponentId fail");
-            return;
-        }
-    
-        std::string id(idStr);
-        auto context = PluginManager::GetInstance();
-        if ((context != nullptr) && (nativeXComponent != nullptr)) 
-        {
-            context->SetNativeXComponent(id, nativeXComponent);
-//             auto render = context->GetRender(id);
-//             if (render != nullptr) 
-//             {
-// //                 render->RegisterCallback(nativeXComponent);
-// //                 render->Export(env, exports);
-//             } else 
-//             {
-// //                 DRAWING_LOGE("render is nullptr");
-//             }
-        }
-        // 初始化 OH_NativeXComponent_Callback
-        // must use static, otherwise 'Ability onDestroy' will deconstruct this member, so xcomponent destroy
-        // cannot callback on this function
-        static OH_NativeXComponent_Callback callback;
-        callback.OnSurfaceCreated = OnSurfaceCreatedCB;
-        callback.OnSurfaceChanged = OnSurfaceChangedCB;
-        callback.OnSurfaceDestroyed = OnSurfaceDestroyedCB;
-        callback.DispatchTouchEvent = DispatchTouchEventCB;
-    
-        // 注册回调函数
-        // after register this callback, kill app will not log 'Ability onWindowStageDestroy' and 'Ability onDestroy'
-        // will crash after kill app
-        OH_NativeXComponent_RegisterCallback(nativeXComponent, &callback);
-    }
+void PluginManager::Export(napi_env env, napi_value exports) {
+	OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "Export: PluginManager");
+	if ((env == nullptr) || (exports == nullptr)) {
+		OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, "mytest", "Export: env or exports is null");
+		return;
+	}
 
-    void PluginManager::SetNativeXComponent(std::string &id, OH_NativeXComponent *nativeXComponent)
-    {
-        nativeXComponentMap_[id] = nativeXComponent;
-    }
+	napi_value exportInstance = nullptr;
+	if (napi_get_named_property(env, exports, OH_NATIVE_XCOMPONENT_OBJ, &exportInstance) != napi_ok) {
+		OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, "mytest", "Export: napi_get_named_property fail");
+		return;
+	}
 
-    void PluginManager::SetNativeWindow(std::string &id, OHNativeWindow *nativeWindow)
-    {
-        pluginRenderMap_[id] = nativeWindow;
-    }
+	OH_NativeXComponent *nativeXComponent = nullptr;
+	if (napi_unwrap(env, exportInstance, reinterpret_cast<void **>(&nativeXComponent)) != napi_ok) {
+		OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, "mytest", "Export: napi_unwrap fail");
+		return;
+	}
 
-    OHNativeWindow* PluginManager::GetRender(std::string &id)
-    {
-        return pluginRenderMap_[id];
-    }
+	char idStr[OH_XCOMPONENT_ID_LEN_MAX + 1] = {'\0'};
+	uint64_t idSize = OH_XCOMPONENT_ID_LEN_MAX + 1;
+	if (OH_NativeXComponent_GetXComponentId(nativeXComponent, idStr, &idSize) != OH_NATIVEXCOMPONENT_RESULT_SUCCESS) {
+		OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, "mytest", "Export: OH_NativeXComponent_GetXComponentId fail");
+		return;
+	}
 
-    // 定义回调函数
-    void OnSurfaceCreatedCB(OH_NativeXComponent* component, void* window)
-    {
-//         OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "OnSurfaceCreatedCB");
-        char idStr[OH_XCOMPONENT_ID_LEN_MAX + 1] = {'\0'};
-        uint64_t idSize = OH_XCOMPONENT_ID_LEN_MAX + 1;
-        if (OH_NativeXComponent_GetXComponentId(component, idStr, &idSize) != OH_NATIVEXCOMPONENT_RESULT_SUCCESS) 
-        {
-//             DRAWING_LOGE("Export: OH_NativeXComponent_GetXComponentId fail");
-            return;
-        }
-        std::string id(idStr);
-        // 可获取 OHNativeWindow 实例
-        OHNativeWindow* nativeWindow = static_cast<OHNativeWindow*>(window);
-        auto context = PluginManager::GetInstance();
-        context->SetNativeWindow(id, nativeWindow);
-        OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "OnSurfaceCreatedCB, id=%{public}s", id.c_str());
-    }
-    void OnSurfaceChangedCB(OH_NativeXComponent* component, void* window)
-    {
-        // 可获取 OHNativeWindow 实例
-        OHNativeWindow* nativeWindow = static_cast<OHNativeWindow*>(window);
-        // ...
-    }
-    void OnSurfaceDestroyedCB(OH_NativeXComponent* component, void* window)
-    {
-        char idStr[OH_XCOMPONENT_ID_LEN_MAX + 1] = {'\0'};
-        uint64_t idSize = OH_XCOMPONENT_ID_LEN_MAX + 1;
-        if (OH_NativeXComponent_GetXComponentId(component, idStr, &idSize) != OH_NATIVEXCOMPONENT_RESULT_SUCCESS) 
-        {
-            return;
-        }
-        OHNativeWindow* nativeWindow = static_cast<OHNativeWindow*>(window);
-        // ...
-        std::string id(idStr);
-        OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "OnSurfaceDestroyedCB, id=%{public}s", id.c_str());
-    }
-    void DispatchTouchEventCB(OH_NativeXComponent* component, void* window)
-    {
-        // 可获取 OHNativeWindow 实例
-        OHNativeWindow* nativeWindow = static_cast<OHNativeWindow*>(window);
-        // ...
-    }
+	std::string id(idStr);
+	auto context = PluginManager::GetInstance();
+	if ((context != nullptr) && (nativeXComponent != nullptr)) {
+		context->SetNativeXComponent(id, nativeXComponent);
+		//             auto render = context->GetRender(id);
+		//             if (render != nullptr)
+		//             {
+		// //                 render->RegisterCallback(nativeXComponent);
+		// //                 render->Export(env, exports);
+		//             } else
+		//             {
+		// //                 DRAWING_LOGE("render is nullptr");
+		//             }
+	}
+	// 初始化 OH_NativeXComponent_Callback
+	// must use static, otherwise 'Ability onDestroy' will deconstruct this member, so xcomponent destroy
+	// cannot callback on this function
+	static OH_NativeXComponent_Callback callback;
+	callback.OnSurfaceCreated = OnSurfaceCreatedCB;
+	callback.OnSurfaceChanged = OnSurfaceChangedCB;
+	callback.OnSurfaceDestroyed = OnSurfaceDestroyedCB;
+	callback.DispatchTouchEvent = DispatchTouchEventCB;
 
-    ohosRender::ohosRender(std::string& id, OHNativeWindow* nativeWindow)
-    {
-        // 设置 OHNativeWindowBuffer 的宽高
-        int32_t code = SET_BUFFER_GEOMETRY;
-        int32_t width = 1080;
-        int32_t height = 1920;
-        // 这里的nativeWindow是从上一步骤中的回调函数中获得的
-        int32_t ret = OH_NativeWindow_NativeWindowHandleOpt(nativeWindow, code, width, height);
-    
-        code = SET_FORMAT;
-        int32_t format = NATIVEBUFFER_PIXEL_FMT_YCBCR_420_P;
-        ret = OH_NativeWindow_NativeWindowHandleOpt(nativeWindow, code, format);
-    
-        OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "set window format=%{public}d", ret);
-    
-        code = SET_TRANSFORM;
-        int32_t transform = NATIVEBUFFER_ROTATE_NONE;
-        ret = OH_NativeWindow_NativeWindowHandleOpt(nativeWindow, code, transform);
-        OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "SET_TRANSFORM=%{public}d", ret);
-        
-        // SET_STRIDE will cause unknown error when OH_NativeWindow_NativeWindowRequestBuffer
-//         code = SET_STRIDE;
-//         int32_t stride = 1920;
-//         ret = OH_NativeWindow_NativeWindowHandleOpt(nativeWindow, code, &stride);
-//         OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "SET_STRIDE=%{public}d", ret);
-        
-        native_window_ = nativeWindow;
-    }
-
-    void ohosRender::OnFrame(uint8_t* data, int width, int height, int stride, int size)
-    {
-        OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "ohosRender::OnFrame");
-        OHNativeWindowBuffer* buffer = nullptr;
-        int fenceFd;
-        int32_t ret = -1;
-        // 通过 OH_NativeWindow_NativeWindowRequestBuffer 获取 OHNativeWindowBuffer 实例
-        ret = OH_NativeWindow_NativeWindowRequestBuffer(native_window_, &buffer, &fenceFd);
-        if (ret != 0)
-        {
-            OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, "mytest", "OH_NativeWindow_NativeWindowRequestBuffer failed, ret=%{public}d", ret);
-            return;
-        }
-        // 通过 OH_NativeWindow_GetBufferHandleFromNative 获取 buffer 的 handle
-        BufferHandle* bufferHandle = OH_NativeWindow_GetBufferHandleFromNative(buffer);
-
-        // 使用内存映射函数mmap将bufferHandle对应的共享内存映射到用户空间，可以通过映射出来的虚拟地址向bufferHandle中写入图像数据
-        // bufferHandle->virAddr是bufferHandle在共享内存中的起始地址，bufferHandle->size是bufferHandle在共享内存中的内存占用大小
-        void* mappedAddr = mmap(bufferHandle->virAddr, bufferHandle->size, PROT_READ | PROT_WRITE, MAP_SHARED, bufferHandle->fd, 0);
-        if (mappedAddr == MAP_FAILED) {
-            // mmap failed
-        }
-    
-        OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "OnFrame::buffer width=%{public}d, height=%{public}d, stride=%{public}d, size=%{public}d", 
-    bufferHandle->width, bufferHandle->height, bufferHandle->stride, bufferHandle->size);
-    
-//         static uint32_t value = 0x00CBC0FF;
-//         value++;
-//         uint32_t *pixel = static_cast<uint32_t *>(mappedAddr); // 使用mmap获取到的地址来访问内存
-//         for (uint32_t x = 0; x < width; x++) {
-//             for (uint32_t y = 0;  y < height; y++) {
-//                 *pixel++ = value;
-//             }
-//         }
-        uint8_t *pixel = static_cast<uint8_t *>(mappedAddr);
-        uint8_t *src_data = static_cast<uint8_t *>(data);
-//         for (uint32_t i = 0; i < size; i++)
-//         {
-//             *pixel++ = *src_data++;
-//         }
-        int byte_align = bufferHandle->stride - stride;
-        for (uint32_t i = 0; i < height; i++)
-        {
-            uint8_t* pos = pixel + i * bufferHandle->stride;
-            for (uint32_t j = 0; j < stride; j++)
-            {
-                *pos++ = *src_data++;
-            }
-        }
-        
-        for (uint32_t i = 0; i < height / 2; i++)
-        {
-            uint8_t* pos = pixel + (i + height) * bufferHandle->stride;
-            for (uint32_t j = 0; j < stride / 2; j++)
-            {
-                *pos++ = *src_data++;
-            }
-            pos += byte_align / 2;
-            for (uint32_t j = 0; j < stride / 2; j++)
-            {
-                *pos++ = *src_data++;
-            }
-        }
-    
-        // 设置刷新区域，如果Region中的Rect为nullptr,或者rectNumber为0，则认为OHNativeWindowBuffer全部有内容更改。
-        Region region{nullptr, 0};
-        // 通过OH_NativeWindow_NativeWindowFlushBuffer 提交给消费者使用，例如：显示在屏幕上。
-        OH_NativeWindow_NativeWindowFlushBuffer(native_window_, buffer, fenceFd, region);
-    
-        // 内存使用完记得去掉内存映射
-        int result = munmap(mappedAddr, bufferHandle->size);
-        if (result == -1) {
-            // munmap failed
-        }
-    }
+	// 注册回调函数
+	// after register this callback, kill app will not log 'Ability onWindowStageDestroy' and 'Ability
+	// onDestroy' will crash after kill app
+	OH_NativeXComponent_RegisterCallback(nativeXComponent, &callback);
 }
+
+void PluginManager::SetNativeXComponent(std::string &id, OH_NativeXComponent *nativeXComponent) {
+	nativeXComponentMap_[id] = nativeXComponent;
 }
+
+void PluginManager::SetNativeWindow(std::string &id, OHNativeWindow *nativeWindow) {
+	pluginRenderMap_[id] = nativeWindow;
+}
+
+OHNativeWindow *PluginManager::GetRender(std::string &id) { return pluginRenderMap_[id]; }
+
+// 定义回调函数
+void OnSurfaceCreatedCB(OH_NativeXComponent *component, void *window) {
+	//         OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "OnSurfaceCreatedCB");
+	char idStr[OH_XCOMPONENT_ID_LEN_MAX + 1] = {'\0'};
+	uint64_t idSize = OH_XCOMPONENT_ID_LEN_MAX + 1;
+	if (OH_NativeXComponent_GetXComponentId(component, idStr, &idSize) != OH_NATIVEXCOMPONENT_RESULT_SUCCESS) {
+		//             DRAWING_LOGE("Export: OH_NativeXComponent_GetXComponentId fail");
+		return;
+	}
+	std::string id(idStr);
+	// 可获取 OHNativeWindow 实例
+	OHNativeWindow *nativeWindow = static_cast<OHNativeWindow *>(window);
+	auto context = PluginManager::GetInstance();
+	context->SetNativeWindow(id, nativeWindow);
+	OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "OnSurfaceCreatedCB, id=%{public}s", id.c_str());
+}
+void OnSurfaceChangedCB(OH_NativeXComponent *component, void *window) {
+	// 可获取 OHNativeWindow 实例
+	OHNativeWindow *nativeWindow = static_cast<OHNativeWindow *>(window);
+	// ...
+}
+void OnSurfaceDestroyedCB(OH_NativeXComponent *component, void *window) {
+	char idStr[OH_XCOMPONENT_ID_LEN_MAX + 1] = {'\0'};
+	uint64_t idSize = OH_XCOMPONENT_ID_LEN_MAX + 1;
+	if (OH_NativeXComponent_GetXComponentId(component, idStr, &idSize) != OH_NATIVEXCOMPONENT_RESULT_SUCCESS) {
+		return;
+	}
+	OHNativeWindow *nativeWindow = static_cast<OHNativeWindow *>(window);
+	// ...
+	std::string id(idStr);
+	OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "OnSurfaceDestroyedCB, id=%{public}s", id.c_str());
+}
+void DispatchTouchEventCB(OH_NativeXComponent *component, void *window) {
+	// 可获取 OHNativeWindow 实例
+	OHNativeWindow *nativeWindow = static_cast<OHNativeWindow *>(window);
+	// ...
+}
+
+ohosRender::ohosRender(std::string &id, OHNativeWindow *nativeWindow) {
+	// 设置 OHNativeWindowBuffer 的宽高
+	int32_t code = SET_BUFFER_GEOMETRY;
+	int32_t width = 1080;
+	int32_t height = 1920;
+	// 这里的nativeWindow是从上一步骤中的回调函数中获得的
+	int32_t ret = OH_NativeWindow_NativeWindowHandleOpt(nativeWindow, code, width, height);
+
+	code = SET_FORMAT;
+	int32_t format = NATIVEBUFFER_PIXEL_FMT_YCBCR_420_P;
+	ret = OH_NativeWindow_NativeWindowHandleOpt(nativeWindow, code, format);
+
+	OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "set window format=%{public}d", ret);
+
+	code = SET_TRANSFORM;
+	int32_t transform = NATIVEBUFFER_ROTATE_NONE;
+	ret = OH_NativeWindow_NativeWindowHandleOpt(nativeWindow, code, transform);
+	OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "SET_TRANSFORM=%{public}d", ret);
+
+	// SET_STRIDE will cause unknown error when OH_NativeWindow_NativeWindowRequestBuffer
+	//         code = SET_STRIDE;
+	//         int32_t stride = 1920;
+	//         ret = OH_NativeWindow_NativeWindowHandleOpt(nativeWindow, code, &stride);
+	//         OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "SET_STRIDE=%{public}d", ret);
+
+	native_window_ = nativeWindow;
+}
+
+void ohosRender::OnFrame(uint8_t *data, int width, int height, int stride, int size, uint32_t orientation) {
+	OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "ohosRender::OnFrame");
+	if (width != width_ || height != height_ || orientation != orientation_) {
+		width_ = width;
+		height_ = height;
+		orientation_ = orientation;
+		ReConfigure();
+	}
+
+	OHNativeWindowBuffer *buffer = nullptr;
+	int fenceFd;
+	int32_t ret = -1;
+	// 通过 OH_NativeWindow_NativeWindowRequestBuffer 获取 OHNativeWindowBuffer 实例
+	ret = OH_NativeWindow_NativeWindowRequestBuffer(native_window_, &buffer, &fenceFd);
+	if (ret != 0) {
+		OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, "mytest",
+					 "OH_NativeWindow_NativeWindowRequestBuffer failed, ret=%{public}d", ret);
+		return;
+	}
+	// 通过 OH_NativeWindow_GetBufferHandleFromNative 获取 buffer 的 handle
+	BufferHandle *bufferHandle = OH_NativeWindow_GetBufferHandleFromNative(buffer);
+
+	// 使用内存映射函数mmap将bufferHandle对应的共享内存映射到用户空间，可以通过映射出来的虚拟地址向bufferHandle中写入图像数据
+	// bufferHandle->virAddr是bufferHandle在共享内存中的起始地址，bufferHandle->size是bufferHandle在共享内存中的内存占用大小
+	void *mappedAddr =
+		mmap(bufferHandle->virAddr, bufferHandle->size, PROT_READ | PROT_WRITE, MAP_SHARED, bufferHandle->fd, 0);
+	if (mappedAddr == MAP_FAILED) {
+		// mmap failed
+	}
+
+	OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest",
+				 "OnFrame::buffer width=%{public}d, height=%{public}d, stride=%{public}d, size=%{public}d",
+				 bufferHandle->width, bufferHandle->height, bufferHandle->stride, bufferHandle->size);
+
+	//         static uint32_t value = 0x00CBC0FF;
+	//         value++;
+	//         uint32_t *pixel = static_cast<uint32_t *>(mappedAddr); // 使用mmap获取到的地址来访问内存
+	//         for (uint32_t x = 0; x < width; x++) {
+	//             for (uint32_t y = 0;  y < height; y++) {
+	//                 *pixel++ = value;
+	//             }
+	//         }
+	uint8_t *pixel = static_cast<uint8_t *>(mappedAddr);
+	uint8_t *src_data = static_cast<uint8_t *>(data);
+	//         for (uint32_t i = 0; i < size; i++)
+	//         {
+	//             *pixel++ = *src_data++;
+	//         }
+	int byte_align = bufferHandle->stride - stride;
+	for (uint32_t i = 0; i < height; i++) {
+		uint8_t *pos = pixel + i * bufferHandle->stride;
+		for (uint32_t j = 0; j < stride; j++) {
+			*pos++ = *src_data++;
+		}
+	}
+
+	for (uint32_t i = 0; i < height / 2; i++) {
+		uint8_t *pos = pixel + (i + height) * bufferHandle->stride;
+		for (uint32_t j = 0; j < stride / 2; j++) {
+			*pos++ = *src_data++;
+		}
+		pos += byte_align / 2;
+		for (uint32_t j = 0; j < stride / 2; j++) {
+			*pos++ = *src_data++;
+		}
+	}
+
+	// 设置刷新区域，如果Region中的Rect为nullptr,或者rectNumber为0，则认为OHNativeWindowBuffer全部有内容更改。
+	Region region{nullptr, 0};
+	// 通过OH_NativeWindow_NativeWindowFlushBuffer 提交给消费者使用，例如：显示在屏幕上。
+	OH_NativeWindow_NativeWindowFlushBuffer(native_window_, buffer, fenceFd, region);
+
+	// 内存使用完记得去掉内存映射
+	int result = munmap(mappedAddr, bufferHandle->size);
+	if (result == -1) {
+		// munmap failed
+	}
+}
+
+void ohosRender::ReConfigure() {
+	int32_t code = SET_BUFFER_GEOMETRY;
+	// 这里的nativeWindow是从上一步骤中的回调函数中获得的
+	int32_t ret = OH_NativeWindow_NativeWindowHandleOpt(native_window_, code, width_, height_);
+
+	code = SET_FORMAT;
+	int32_t format = NATIVEBUFFER_PIXEL_FMT_YCBCR_420_P;
+	ret = OH_NativeWindow_NativeWindowHandleOpt(native_window_, code, format);
+	OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "set window format=%{public}d", ret);
+
+	code = SET_TRANSFORM;
+	int32_t transform = NATIVEBUFFER_ROTATE_NONE;
+	if (orientation_ == 90) {
+		transform = NATIVEBUFFER_ROTATE_270;
+	} else if (orientation_ == 270) {
+		transform = NATIVEBUFFER_ROTATE_90;
+	}
+	// if mirror, use NATIVEBUFFER_FLIP_V_ROT90
+	ret = OH_NativeWindow_NativeWindowHandleOpt(native_window_, code, transform);
+	OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "mytest", "SET_TRANSFORM=%{public}d", ret);
+}
+} // namespace ohos
+} // namespace webrtc
